f15c8072e7f608d7e8b307b2786039628e1440d6
Joseph Anderson <jtanderson31@gmail.com>
2015-03-18 15:46:21 -0400

    Support markdown/html and add preview on admin panel

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -2,3 +2,7 @@ Template.registerHelper("isAdmin", function(){
 	// return Meteor.user() && Roles.userIsInRole(Meteor.user()._id, 'admin');
 	return isAdmin();
 });
+
+Template.registerHelper("processMarkdown", function(){
+	return marked(this);
+});
