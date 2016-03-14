f520cc7cf992b72ddc791c5f122093cb85247b77
Joseph Anderson <jtanderson@ratiocaeli.com>
2016-03-14 14:44:53 -0400

    Fix up migrations

--- client/application.js
+++ client/application.js
@@ -1,120 +1,120 @@
 // Subscriptions, Meteor.startup
 
 Tracker.autorun(function(){
-	// Meteor.subscribe("counter");
+  // Meteor.subscribe("counter");
 });
 
 Template.lottery.helpers({
-	current: function(){
-		var lotto = Lotteries.current();
-		return lotto;
-	}
+  current: function(){
+    var lotto = Lotteries.current();
+    return lotto;
+  }
 });
 
 Template.editLottery.events({
-	'submit form': function(e){
-		e.preventDefault();
-	}
+  'submit form': function(e){
+    e.preventDefault();
+  }
 });
 
 Template.addLotteryUser.events({
-	'submit form': function(e){
-		e.preventDefault();
+  'submit form': function(e){
+    e.preventDefault();
 
-		var user = {
-			lottery_id: this._id,
-			name: $(e.target).find('[name=name]').val(),
-			entry: $(e.target).find('[name=entry]').val()
-		};
+    var user = {
+      lottery_id: this._id,
+      name: $(e.target).find('[name=name]').val(),
+      entry: $(e.target).find('[name=entry]').val()
+    };
 
-		LotteryUsers.insert(user);
-		$(e.target).find('[name=name]').val('');
-		$(e.target).find('[name=entry]').val(this.defaultEntryAmount || '');
-	}
+    LotteryUsers.insert(user);
+    $(e.target).find('[name=name]').val('');
+    $(e.target).find('[name=entry]').val(this.defaultEntryAmount || '');
+  }
 });
 
 Template.lotteryCreate.events({
-	'submit form': function(e){
-		e.preventDefault();
+  'submit form': function(e){
+    e.preventDefault();
 
-		var id = Lotteries.insert({
-			endDate: $(e.target).find('[name=endDate]').val(),
-			active: true,
-			onHomePage: false,
-			bonus: $(e.target).find('[name=bonus]').val(),
-			defaultEntryAmount: $(e.target).find('[name=defaultEntryAmount]').val()
-		});
+    var id = Lotteries.insert({
+      endDate: $(e.target).find('[name=endDate]').val(),
+      active: true,
+      onHomePage: false,
+      bonus: $(e.target).find('[name=bonus]').val(),
+      defaultEntryAmount: $(e.target).find('[name=defaultEntryAmount]').val()
+    });
 
-		Router.go('/lottery/'+id);
-	}
+    Router.go('/lottery/'+id);
+  }
 });
 
 Template.showLottery.events({
-	'click .delete-user': function(e){
-		e.preventDefault();
-		LotteryUsers.remove(this._id);
-	},
-	'click a.btn[name=winnerbutton]': function(e){
-		e.preventDefault();
-		this.selectWinner();
-	}
+  'click .delete-user': function(e){
+    e.preventDefault();
+    LotteryUsers.remove(this._id);
+  },
+  'click a.btn[name=winnerbutton]': function(e){
+    e.preventDefault();
+    this.selectWinner();
+  }
 });
 
 Template.showLottery.helpers({
-	userIsWinner: function(){
-		if ( this.winner ){
-			return "alert alert-success winner";
-			// return "winner";
-		}
-	}
+  userIsWinner: function(){
+    if ( this.winner ){
+      return "alert alert-success winner";
+      // return "winner";
+    }
+  }
 })
 
 Template.editComments.events({
-	'submit form': function(e){
-		e.preventDefault();
+  'submit form': function(e){
+    e.preventDefault();
 
-		Lotteries.update(this._id, {$set: {comments: $(e.target).find('textarea').val() }});
-	}
+    Lotteries.update(this._id, {$set: {comments: $(e.target).find('textarea').val() }});
+  }
 });
 
 Template.admin.events({
-	'click a.btn[name=delete]': function(e){
-		e.preventDefault();
+  'click a.btn[name=delete]': function(e){
+    e.preventDefault();
 
-		if ( confirm("Are you sure you want to delete this lottery?") ){
-			Lotteries.remove(this._id);
-		}
-	},
-	'change input[name=onHomePageRadio]': function(e){
-		e.preventDefault();
+    if ( confirm("Are you sure you want to delete this lottery?") ){
+      Lotteries.remove(this._id);
+    }
+  },
+  'change input[name=onHomePageRadio]': function(e){
+    e.preventDefault();
 
-		if ( $(e.target).is(":checked") ){
-			Meteor.call("addLotteryToHomePage", this._id);
-		} else {
-			Lotteries.update(this._id, {$set: {onHomePage: false}});
-		}
-	},
-	'change input[name=adminRadio]': function(e){
-		e.preventDefault();
-		if ( Roles.userIsInRole(this._id, 'admin') ){
-			Roles.removeUsersFromRoles(this._id, 'admin');
-		} else {
-			Roles.addUsersToRoles(this._id, 'admin');
-		}
-	}
+    if ( $(e.target).is(":checked") ){
+      Meteor.call("addLotteryToHomePage", this._id);
+    } else {
+      Lotteries.update(this._id, {$set: {onHomePage: false}});
+    }
+  },
+  'change input[name=adminRadio]': function(e){
+    e.preventDefault();
+    if ( Roles.userIsInRole(this._id, 'admin') ){
+      Roles.removeUsersFromRoles(this._id, 'admin');
+    } else {
+      Roles.addUsersToRoles(this._id, 'admin');
+    }
+  }
 });
 
 Template.admin.helpers({
-	users: function(){
-		return Meteor.users.find().fetch();
-	},
-	userEmail: function(user){
-		return this.emails[0].address;
-	}
+  users: function(){
+    return Meteor.users.find().fetch();
+  },
+  userEmail: function(user){
+    return this.emails[0].address;
+  }
 });
 
 Template.adminToggle.helpers({
-	userIsAdmin: function(){
-		return Roles.userIsInRole(this._id, 'admin');
-	}
+  userIsAdmin: function(){
+    return Roles.userIsInRole(this._id, 'admin');
+  }
 });
--- server/migrations.js
+++ server/migrations.js
@@ -1,68 +1,73 @@
 Migrations.add({
-	version: 1,
-	name: "Set jtanderson31@gmail.com to be an admin user.",
-	up: function(){
-		var user = Meteor.users.findOne({"emails.address": "jtanderson31@gmail.com"});
-		if ( user && ! Roles.userIsInRole(user, 'admin') ){
-			Roles.addUsersToRoles(user._id, "admin");
-		}
-	}
+  version: 1,
+  name: "Set jtanderson31@gmail.com to be an admin user.",
+  up: function(){
+    Accounts.createUser({
+      "username": "jtanderson31@gmail.com", 
+      "email": "jtanderson31@gmail.com",
+      "password": "firstpass"
+    });
+    var user = Meteor.users.findOne({"emails.address": "jtanderson31@gmail.com"});
+    if ( user && ! Roles.userIsInRole(user, 'admin') ){
+      Roles.addUsersToRoles(user._id, "admin");
+    }
+  }
 });
 
 Migrations.add({
-	version: 2,
-	name: "Add users array to each lottery.",
-	up: function(){
-		for (var i = Lotteries.find().fetch().length - 1; i >= 0; i--) {
-			var lot = Lotteries.find().fetch()[i];
-			if ( ! lot.hasOwnProperty("users") ){
-				Lotteries.update(lot._id, {$set: {users: []}});
-			}
-		};
-	}
+  version: 2,
+  name: "Add users array to each lottery.",
+  up: function(){
+    for (var i = Lotteries.find().fetch().length - 1; i >= 0; i--) {
+      var lot = Lotteries.find().fetch()[i];
+      if ( ! lot.hasOwnProperty("users") ){
+        Lotteries.update(lot._id, {$set: {users: []}});
+      }
+    };
+  }
 });
 
 Migrations.add({
-	version: 3,
-	name: "Add defaultEntryAmount to each lottery.",
-	up: function(){
-		for (var i = Lotteries.find().fetch().length - 1; i >= 0; i--) {
-			var lot = Lotteries.find().fetch()[i];
-			if ( ! lot.hasOwnProperty("defaultEntryAmount") ){
-				Lotteries.update(lot._id, {$set: {defaultEntryAmount: 0}});
-			}
-		};
-	}
+  version: 3,
+  name: "Add defaultEntryAmount to each lottery.",
+  up: function(){
+    for (var i = Lotteries.find().fetch().length - 1; i >= 0; i--) {
+      var lot = Lotteries.find().fetch()[i];
+      if ( ! lot.hasOwnProperty("defaultEntryAmount") ){
+        Lotteries.update(lot._id, {$set: {defaultEntryAmount: 0}});
+      }
+    };
+  }
 });
 
 Migrations.add({
-	version: 4,
-	name: "Move lottery.users to LotteryUsers collection",
-	up: function(){
-		var lotteries = Lotteries.find().fetch();
-		for (var i = lotteries.length - 1; i >= 0; i--) {
-			if ( lotteries[i].users ){
-				for (var j = lotteries[i].users.length - 1; j >= 0; j--) {
-					// lotteries[i].users[j]
-					var lotteryuser = _.clone(lotteries[i].users[j]);
-					_.extend(lotteryuser, {
-						lottery_id: lotteries[i]._id
-					});
-					LotteryUsers.insert(lotteryuser);
-					Lotteries.update(lotteries[i]._id, {$unset: {users: ""}});
-				};
-			}
-		};
-	}
+  version: 4,
+  name: "Move lottery.users to LotteryUsers collection",
+  up: function(){
+    var lotteries = Lotteries.find().fetch();
+    for (var i = lotteries.length - 1; i >= 0; i--) {
+      if ( lotteries[i].users ){
+        for (var j = lotteries[i].users.length - 1; j >= 0; j--) {
+          // lotteries[i].users[j]
+          var lotteryuser = _.clone(lotteries[i].users[j]);
+          _.extend(lotteryuser, {
+            lottery_id: lotteries[i]._id
+          });
+          LotteryUsers.insert(lotteryuser);
+          Lotteries.update(lotteries[i]._id, {$unset: {users: ""}});
+        };
+      }
+    };
+  }
 });
 
 Migrations.add({
-	version: 5,
-	name: "Add field for bonus pool",
-	up: function(){
-		var lotteries = Lotteries.find().fetch();
-		for ( var i = lotteries.length - 1; i >= 0; i--) {
-			Lotteries.update(lotteries[i]._id, {$set: {bonus: 0}});
-		}
-	}
+  version: 5,
+  name: "Add field for bonus pool",
+  up: function(){
+    var lotteries = Lotteries.find().fetch();
+    for ( var i = lotteries.length - 1; i >= 0; i--) {
+      Lotteries.update(lotteries[i]._id, {$set: {bonus: 0}});
+    }
+  }
 });
